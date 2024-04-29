1a889387c901cf9841dc358c7e17e136edc313e5
chenhang <1362320337@qq.com>
2024-04-29 18:05:31 +0800

    试验demo2

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,9 @@
+// app/dashboard/layout.js
+export default function DashboardLayout({children,}) {
+    return (
+        <section>
+            <nav>nav</nav>
+            {children}
+        </section>
+    )
+}
